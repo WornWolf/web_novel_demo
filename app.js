c4ca442ca9564a873391f9ccb09c2d1c88bfc2bb
WornWolf <thanakorn6661@gmail.com>
2025-11-10 21:09:03 +0700

    Update app.js, package.json and add vercel.json

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
-const PORT = 3000;
 const path = require("path");
-const rootDir = require("./util/path");
-const novelRouter = require("./routes/novel");
+const rootDir = path.join(__dirname, "..");
+const novelRouter = require("../routes/novel");
 
 // Set Template Engine
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", path.join(rootDir, "views"));
 
 // Set JSON and body
 app.use(express.json());
@@ -20,13 +20,12 @@ app.use(express.static(path.join(rootDir, "public")));
 app.use(express.static(path.join(rootDir, "assets")));
 
 // use Router
-app.use("/",novelRouter);
+app.use("/", novelRouter);
 
 // 404 Page
 app.use((req, res) => {
-  res.status(404).render("404", {pageTitle: "404", activePage: '404'});
+  res.status(404).render("404", { pageTitle: "404", activePage: "404" });
 });
 
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+module.exports = app;
+module.exports.handler = serverless(app);
