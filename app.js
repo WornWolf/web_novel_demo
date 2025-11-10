93f794007306777cf30a606f7ce77929d4e88c3d
WornWolf <thanakorn6661@gmail.com>
2025-11-10 21:28:09 +0700

    fixed app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const rootDir = path.join(__dirname, "..");
-const novelRouter = require("../routes/novel");
+const novelRouter = require("./routes/novel");
 
 // Set Template Engine
 app.set("view engine", "ejs");
