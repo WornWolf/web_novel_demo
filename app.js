89a4aec69536f2e2cd8b9c0d71420ce05044f917
WornWolf <thanakorn6661@gmail.com>
2025-11-10 21:31:58 +0700

    fixed app.js

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 const path = require("path");
-const rootDir = path.join(__dirname, "..");
+const rootDir = require("./util/path");
 const novelRouter = require("./routes/novel");
 
 // Set Template Engine
 app.set("view engine", "ejs");
-app.set("views", path.join(rootDir, "views"));
+app.set("views", "views");
 
 // Set JSON and body
 app.use(express.json());
@@ -20,11 +20,11 @@ app.use(express.static(path.join(rootDir, "public")));
 app.use(express.static(path.join(rootDir, "assets")));
 
 // use Router
-app.use("/", novelRouter);
+app.use("/",novelRouter);
 
 // 404 Page
 app.use((req, res) => {
-  res.status(404).render("404", { pageTitle: "404", activePage: "404" });
+  res.status(404).render("404", {pageTitle: "404", activePage: '404'});
 });
 
 module.exports = app;
